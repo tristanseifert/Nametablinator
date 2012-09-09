3cd42e4a8dfb73139d73109ffae06e7f0e019b0b
Tristan Seifert <tristan@247server.net>
2012-09-09 13:30:35 -0500

    Initial Commit

---
+++ Nametablinator/SQUDocument.h
@@ -0,0 +1,13 @@
+//
+//  SQUDocument.h
+//  Nametablinator
+//
+//  Created by Tristan Seifert on 09/09/2012.
+//  Copyright (c) 2012 Tristan Seifert. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SQUDocument : NSDocument
+
+@end
---
+++ Nametablinator/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Nametablinator
+//
+//  Created by Tristan Seifert on 09/09/2012.
+//  Copyright (c) 2012 Tristan Seifert. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
